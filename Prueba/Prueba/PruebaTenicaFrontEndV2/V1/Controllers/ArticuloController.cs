da3c3688f0d1cd16c1d5a653e7c6ec642c2f1546
Emanuel94 <emanuel.esquivel94@gmail.com>
2023-12-13 08:37:47 -0600

    Se realiza ajustes para usar WCF

---
+++ Prueba/Prueba/PruebaTenicaFrontEndV2/V1/Controllers/ArticuloController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PruebaTecnica.Model;
+
+namespace PruebaTecnica
+{
+
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    [Produces("application/json")]
+    public class ArticuloController : ControllerBase
+    {
+        #region Atributos
+        private readonly PruebaTecnica.BusinessLogic.Interface.IArticulo businessLogicArticulo;
+
+        #endregion
+
+
+        public ArticuloController()
+        {
+
+        }
+
+        public async Task<IEnumerable<Articulo>> Listar()
+        {
+            try
+            {
+                //businessLogicArticulo.Listar();
+                return null;
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                throw;
+            }
+        }
+    }
+}
---
+++ WCFServiceMedico/WCFServiceMedico/Class/Articulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WCFServiceMedico.Class
+{
+    public class Articulo
+    {
+        [DataMember(Order = 0)]
+        public int IdArticulo { get; set; } 
+
+        [DataMember(Order = 2)]
+        public string Descripcion { get; set; }
+
+
+        [DataMember(Order = 3)]
+        public int IdMarca { get; set; }
+
+        [DataMember(Order = 4)]
+        public string Sku { get; set; }
+
+    }
+}
